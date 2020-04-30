95f4efd6e74fd52182114d4e1bfa7cd6afbd47ed
Dina Katalsheva <katalsheva@yandex.ru>
2020-04-30 11:03:01 +0300

    first commit

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        long initial = 2000000000;
+        long added = 500000000;
+        long result = initial + added;
+        System.out.println(result);
+    }
+}
